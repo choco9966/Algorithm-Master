2dbbf90c86f50ef10e620feded34da5f05bc56c8
kimhyunwoo <37744629+choco9966@users.noreply.github.com>
2020-08-29 19:36:08 +0900

    Create 프린터.py

---
+++ programmers/level2/code/프린터.py
@@ -0,0 +1,18 @@
+def solution(priorities, location):
+    answer = 0
+
+    array1 = [c for c in range(len(priorities))] # index 위치 저장 
+    array2 = priorities.copy() # 값 저장 (출력되는 값)
+
+    i = 0
+    while True:
+        if array2[i] < max(array2[i+1:]):
+            array1.append(array1.pop(i))
+            array2.append(array2.pop(i))
+        else:
+            i += 1
+
+        if array2 == sorted(array2, reverse=True):
+            break
+
+    return array1.index(location) + 1
