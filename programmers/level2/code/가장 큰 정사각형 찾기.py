fe3184f63ad4e4b433e5a8d8868f3e78c031c085
kimhyunwoo <37744629+choco9966@users.noreply.github.com>
2020-08-30 08:29:40 +0900

    Delete 가장 큰 정사각형 찾기.py

--- programmers/level2/code/가장 큰 정사각형 찾기.py
+++
@@ -1,7 +0,0 @@
-def solution(land):
-    for i in range(0, len(land)-1):
-        land[i+1][0] += max(land[i][1],land[i][2],land[i][3])
-        land[i+1][1] += max(land[i][0],land[i][2],land[i][3])
-        land[i+1][2] += max(land[i][0],land[i][1],land[i][3])
-        land[i+1][3] += max(land[i][0],land[i][1],land[i][2])
-    return max(land[len(land)-1])
