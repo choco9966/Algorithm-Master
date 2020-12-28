78ec85d41ac872452aafbca7b7fb9af178a30465
kimhyeonwoo <37744629+choco9966@users.noreply.github.com>
2020-12-28 19:38:58 +0900

    Update 두번째최대값.py

--- elice/데이터 구조/재귀호출 응용 및 힙/두번째최대값.py
+++ elice/데이터 구조/재귀호출 응용 및 힙/두번째최대값.py
@@ -1,22 +1,15 @@
+from itertools import chain
 def getMax2(n, myMatrix):
     '''
     크기 n x n 의 행렬 myMatrix내의 원소의 합, 최댓값, 두 번째 최댓값을 반환하는 함수.
 
     만약 myMatrix = [[1, 2, 3], [2, 3, 4], [3, 3, 4]]라면 (25, 4, 3) 을 반환한다.
     '''
-
-    mySum = 0
-    myMax = 0
-    myMax2 = 0
+    myList = list(chain(*myMatrix))
     
-    for matrix in myMatrix :
-        for num in matrix :
-            mySum += num
-            if myMax2 < num :
-                if myMax < num :
-                    myMax2,myMax = myMax,num
-                elif myMax > num :
-                    myMax2 = num
+    mySum = sum(myList)
+    myMax = max(myList)
+    myMax2 = sorted(set(myList), reverse=True)[1]
 
     return (mySum, myMax, myMax2)
 
