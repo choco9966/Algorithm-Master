def4a050bc74c29b12dc36f1697a81389ec782de
kimhyeonwoo <37744629+choco9966@users.noreply.github.com>
2021-02-12 13:35:07 +0900

    Update 1406번 에디터.py

--- baekjoon/1406번 에디터.py
+++ baekjoon/1406번 에디터.py
@@ -1,3 +1,22 @@
+# 출처 : https://namhandong.tistory.com/113
+import sys 
+s1 = list(sys.stdin.readline().strip())
+s2 = []
+m = int(sys.stdin.readline())
+n = len(s1)
+
+for i in range(m):
+    com = sys.stdin.readline().strip()
+    if com[0] == 'P':
+        s1.append(com[2])
+    elif com[0] == 'L' and s1 != []: 
+        s2.append(s1.pop())
+    elif com[0] == 'D' and s2 != []:
+        s1.append(s2.pop())
+    elif com[0] == 'B' and s1 != []: 
+        s1.pop()
+print("".join(s1 + list(reversed(s2))))
+
 # 시간초과 
 import sys
 from collections import deque
